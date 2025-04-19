414256cadfa3ffe6eea50ac9c9f89f253193c209
Alexandria <alexa42494@gmail.com>
2025-04-19 15:38:21 -0500

    Rename memory init function

--- src/memory.c
+++ src/memory.c
@@ -5,7 +5,7 @@ cutils_malloc_t cutils_malloc = malloc;
 cutils_calloc_t cutils_calloc = calloc;
 cutils_free_t cutils_free = free;
 
-void cutils_init(cutils_malloc_t _malloc, cutils_calloc_t _calloc, cutils_free_t _free)
+void cutils_memory_init(cutils_malloc_t _malloc, cutils_calloc_t _calloc, cutils_free_t _free)
 {
     cutils_malloc = _malloc;
     cutils_calloc = _calloc;
